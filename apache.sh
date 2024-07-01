bc0f11cb784ad254ed27b14161073c099a3e9918
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2024-07-01 23:18:38 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,5 +3,5 @@ yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
 cd /var/www/html
-git clone https://github.com/RAHAMSHAIK007/netflixcodde.git
+git clone https://github.com/vardhan1228/netflixcodde.git
 mv netflixcodde/* .
